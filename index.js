30fe7650e304c7bcf108aa3c7e69d9c164bbe31a
iesratadhara <iesratjahan912@gmail.com>
2022-11-26 07:06:36 +0600

    users api creater

--- index.js
+++ index.js
@@ -13,8 +13,31 @@ app.use(cors())
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ip0tsyu.mongodb.net/?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
  
+async function run(){
+    try{
+        const userCollection = client.db('pushpaliResell').collection('users')
+
+        app.post('/users', async(req, res)=>{
+            const user = req.body
+            const result = await userCollection.insertOne(user)
+            res.send(result)
+        })
+
+        app.get('/users', async(req, res)=>{
+            const queary = {}
+            const result = await userCollection.find(queary).toArray()
+            res.send(result)
+        })
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.log)
 
 app.get('/', async(req,res)=>{
     res.send('Pushpali server is running')
